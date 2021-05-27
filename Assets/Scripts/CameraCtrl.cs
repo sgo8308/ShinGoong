5a351dfe02503795e74a3db0879dc94d7cf39113
jiwoo <sgo8308@naver.com>
2021-05-27 14:56:19 +0900

    0527지우

--- Assets/Scripts/CameraCtrl.cs
+++ Assets/Scripts/CameraCtrl.cs
@@ -7,10 +7,7 @@ public class CameraCtrl : MonoBehaviour
 
     public Camera m_Camera;
     public Transform m_Player;
-    public float camPosXLimitLeft;
-    public float camPosXLimitRight;
-    public float camPosYLimitTop;
-    public float camPosYLimitBot;
+
     void Start()
     {
         
@@ -25,50 +22,7 @@ public class CameraCtrl : MonoBehaviour
     private void FollowPlayer()
     {
         Vector3 position = m_Player.position;
-        //카메라 최대 X좌표 제한두기
-        if (m_Player.position.x < camPosXLimitLeft)
-        {
-            position.Set(camPosXLimitLeft, m_Player.position.y, transform.position.z);
-        }
-        else if (camPosXLimitLeft <= m_Player.position.x && m_Player.position.x <= camPosXLimitRight)
-        {
-            position.Set(m_Player.position.x, m_Player.position.y, transform.position.z);
-        }
-        else if (m_Player.position.x > camPosXLimitRight)
-        {
-            position.Set(camPosXLimitRight, m_Player.position.y, transform.position.z);
-        }
-        //카메라 최대 Y좌표 제한두기
-
-        //왼쪽만 제한
-        //왼쪽 제한, 아래 제한
-        //왼쪽 제한, 위에 제한
-        //오른쪽만 제한
-        //오른쪽 제한, 아래 제한
-        //오른쪽 제한, 위에 제한
-        if (m_Player.position.x < camPosXLimitLeft && (m_Player.position.y >= camPosYLimitBot && m_Player.position.y <= camPosYLimitTop))
-        {
-            position.Set(camPosXLimitLeft, m_Player.position.y, transform.position.z);
-        }
-        else if (m_Player.position.x < camPosXLimitLeft && m_Player.position.y < camPosYLimitBot)
-        {
-            position.Set(camPosXLimitLeft, camPosYLimitBot, transform.position.z);
-        }
-        else if (m_Player.position.x < camPosXLimitLeft && m_Player.position.y < camPosYLimitBot)
-        {
-            position.Set(camPosXLimitLeft, camPosYLimitBot, transform.position.z);
-        }
-
-
-
-        else if (camPosXLimitLeft <= m_Player.position.x && m_Player.position.x <= camPosXLimitRight)
-        {
-            position.Set(m_Player.position.x, m_Player.position.y, transform.position.z);
-        }
-        else if (m_Player.position.x > camPosXLimitRight)
-        {
-            position.Set(camPosXLimitRight, m_Player.position.y, transform.position.z);
-        }
+        position.Set(m_Player.position.x, m_Player.position.y + 5, transform.position.z);
 
         m_Camera.transform.SetPositionAndRotation(position, Quaternion.identity);
     }
