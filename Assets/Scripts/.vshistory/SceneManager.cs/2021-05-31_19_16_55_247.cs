9e93fad81510686709f572094a3fa65afeb98f75
jiwoo <sgo8308@naver.com>
2021-05-31 19:30:56 +0900

    시작 씬 구현완료

---
+++ Assets/Scripts/.vshistory/SceneManager.cs/2021-05-31_19_16_55_247.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    void goTo(string sceneName)
+    {
+        SceneManager.LoadScene("ShelterScene");
+    }
+}
