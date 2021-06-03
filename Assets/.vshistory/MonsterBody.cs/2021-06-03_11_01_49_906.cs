05d77fb391101e02c8d39a075071e38511020ab2
jiwoo <sgo8308@naver.com>
2021-06-03 18:12:11 +0900

    몬스터 죽음 구현
    - 몬스터가 죽으면 꽂혀 있던 화살이 바닥에 떨어짐
    - 바닥에 떨어진 화살을 주울 수 있음

---
+++ Assets/.vshistory/MonsterBody.cs/2021-06-03_11_01_49_906.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBody : MonoBehaviour
+{
+    MonsterMite _monsterMite; 
+    private void Start()
+    {
+        _monsterMite = this.GetComponentInParent<MonsterMite>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "arrow")
+        {
+            collision.gameObject.transform.parent = this.transform;
+
+            Arrow arrow = collision.gameObject.GetComponent<Arrow>();
+            GetAngry();
+
+            Hit(arrow.GetDamage());
+
+            CancelInvoke("GetPeaceful");
+            Invoke("GetPeaceful", 5);
+        }
+    }
+}
---
+++ Assets/.vshistory/MonsterBody.cs/2021-06-03_13_14_47_820.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class MyCollisionEvent : UnityEvent<Collision2D>
+{
+}
+public class MonsterBody : MonoBehaviour
+{
+    public MyCollisionEvent OnMonsterHit;
+
+    private void Awake()
+    {
+        if(OnMonsterHit == null)
+            OnMonsterHit = new MyCollisionEvent();
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        OnMonsterHit.Invoke(collision);
+    }
+
+
+}
