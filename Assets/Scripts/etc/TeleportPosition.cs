26e98a40c7a22f3e496467f086aee7026a7e0d38
jiwoo <sgo8308@naver.com>
2021-06-26 15:27:43 +0900

    텔레포트 버그 수정
    - 텔레포트 화살 쏘고 텔레포트 화살 오프해서 텔레포트 안됐는데 다시 키면 그 자리로 텔레포트 되는 버그 수정

---
+++ Assets/Scripts/etc/TeleportPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPosition : MonoBehaviour
+{
+    bool isInvokeStart;
+    void Update()
+    {
+        if (!transform.parent && !isInvokeStart)
+        {
+            Invoke("DestroyThis", 0.1f);
+            isInvokeStart = true;
+        }
+    }
+
+    private void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+}
